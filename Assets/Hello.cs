af864871779ae62d5195202bda6c812cb0e8dbb8
Puifaii05 <Kanyaluk.phen@bumail.net>
2025-03-03 23:42:06 +0700

    Add Hello Script

---
+++ Assets/Hello.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
